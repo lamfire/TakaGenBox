1abcef26a8d7f47f088685553b3d43446a114887
lamfire <hayash@163.com>
2025-09-05 22:33:15 +0800

    update 20250905

---
+++ build.bat
@@ -0,0 +1,10 @@
+@if "%DEBUG%" == "" @echo off
+@rem ##########################################################################
+@rem
+@rem  Gradle startup script for Windows
+@rem
+@rem ##########################################################################
+
+gradlew.bat assembleRelease
+
+pause
